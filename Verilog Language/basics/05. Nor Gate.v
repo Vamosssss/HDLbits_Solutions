ac0ad0e62d6f2bbef4d3e97b597a8a9042f4ef09
just for study <85097140+cxcx9491@users.noreply.github.com>
2024-07-28 11:41:18 +0900

    Delete Verilog Language directory

--- Verilog Language/basics/05. Nor Gate.v
+++
@@ -1,9 +0,0 @@
-module top_module( 
-    input a, 
-    input b, 
-    output out );
-
-    assign out = ~(a|b);
-  //nor(out,a,b);
-  
-endmodule
--- Verilog Language/basics/06. Xnor Gate.v
+++
@@ -1,9 +0,0 @@
-module top_module( 
-    input a, 
-    input b, 
-    output out );
-    
-    assign out = ~(a^b);
-  //xnor(out,a,b);
-
-endmodule
