c8c3273022f0d9a5c9f2544ecb2b87e30135f15d
just for study <85097140+Vamosssss@users.noreply.github.com>
2024-08-01 09:02:43 +0900

    Update 8. Carry-select adder.ver2.v

--- 2. Verilog Language/3.Modules:Hierarchy/8. Carry-select adder.ver2.v
+++ 2. Verilog Language/3.Modules:Hierarchy/8. Carry-select adder.ver2.v
@@ -3,17 +3,20 @@ module top_module(
     input [31:0] b,
     output reg [31:0] sum
 );
-    wire [15:0] cout,con2;
+    wire [15:0] cout,cout2;
     wire [15:0] sum1, sum2;
   add16 inst1(a[15:0], b[15:0], 0, sum[15:0], cout);
-  add16 inst2(a[31:16], b[31:16], 0, sum1, con2);
-  add16 inst3(a[31:16], b[31:16], 1, sum2, con2);
+    add16 inst2(a[31:16], b[31:16], 0, sum1, cout2);
+    add16 inst3(a[31:16], b[31:16], 1, sum2, cout2);
     
-    always @(cout, alt_sum1, alt_sum2) begin
+    always @(cout,sum1,sum2) begin
         case(cout)
             0 : sum[31:16] = sum1;
             1 : sum[31:16] = sum2;
         endcase
     end
+
+endmodule
+
 
 endmodule
