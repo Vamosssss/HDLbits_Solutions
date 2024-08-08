85d8fde792ba75f2be3805b30d6a481ce578ff35
just for study <85097140+Vamosssss@users.noreply.github.com>
2024-08-09 08:21:15 +0900

    Create 02. Full adder.v

---
+++ 3. Circuit/1. Combinational Logic/3. Arithmetic Circuits/02. Full adder.v
@@ -0,0 +1,10 @@
+module top_module( 
+    input a, b, cin,
+    output cout, sum )
+    
+    assign sum = a ^ b ^ cin;
+    assign cout = (a & b) | (a & cin) | (b & cin);
+
+  //assign {cout,sum} = a + b + cin;
+
+endmodule
