c407604d1e51fd7fa43ef22c8bbae40169593855
just for study <85097140+Vamosssss@users.noreply.github.com>
2024-10-04 15:00:09 +0900

    Update 11. Simple Circuit B.v

--- 3. Circuit/1. Combinational Logic/1. Basic Gates/11. Simple Circuit B.v
+++ 3. Circuit/1. Combinational Logic/1. Basic Gates/11. Simple Circuit B.v
@@ -1,6 +1,16 @@
 module top_module ( input x, input y, output z );
     
     assign z = ~(x^y);
+    
     //xnor(z,x,y);
+
+    /* always @(*) begin
+        case({x,y})
+            2'b00 : z = 1;
+            2'b10 : z = 0;
+            2'b01 : z = 0;
+            2'b11 : z = 1;
+        endcase
+    end*/
 
 endmodule
