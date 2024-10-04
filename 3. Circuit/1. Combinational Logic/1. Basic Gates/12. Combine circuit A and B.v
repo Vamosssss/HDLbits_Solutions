50710ba73f4495e7c75eb8657f97bcde53a93f21
just for study <85097140+Vamosssss@users.noreply.github.com>
2024-10-04 15:12:26 +0900

    Update 12. Combine circuit A and B.v

--- 3. Circuit/1. Combinational Logic/1. Basic Gates/12. Combine circuit A and B.v
+++ 3. Circuit/1. Combinational Logic/1. Basic Gates/12. Combine circuit A and B.v
@@ -10,6 +10,15 @@ module top_module (input x, input y, output z);
     assign z5 = z1 || z2;
     assign z6 = z3 && z4;
     assign z = z5 ^ z6;
+
+    /*
+     wire z1, z2, z3, z4;
+    A IA1(x,y,z1);
+    A IA2(x,y,z2);
+    B IB1(x,y,z3);
+    B IB2(x,y,z4);
+    
+    assign z = (z1|z2)^(z3&z4); */
     
 endmodule
 
